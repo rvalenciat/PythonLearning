409b96c589aee78e7b7a2a0496233924299f4e65
Rodolfo Valencia <rlvalencia@sodimac.cl>
2020-09-05 13:05:06 -0400

    Âadded python files from macbook

---
+++ web_scrapping.py
@@ -0,0 +1,28 @@
+import pandas as pd
+
+def clean_df(df):
+    df = df[0]
+    del df["Mapa administrativo"]
+    del df["Capital"]
+    
+    return df
+
+def clean_titles(df):
+    df.rename(columns={
+        "Región":"Region",
+        "Población [8]​":"Poblacion",
+        "Superficie (km²)[4]​":"Superficie"
+    }, inplace=True)
+
+    return df
+
+url = "https://es.wikipedia.org/wiki/Chile"
+
+df = pd.read_html(url, attrs={"class":"wikitable col1izq col2der col3der col4der col5der col6izq"}, header=1)
+
+df = clean_df(df)
+df = clean_titles(df)
+
+print(df)
+
+df.replace()
